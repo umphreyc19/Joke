9ea25175dd9349bf232651eee6e75ea15bf96f4e
Collin Beauchamp-Umphrey <umphreyc19@hanover.edu>
2018-05-17 12:00:28 -0400

    A lot of stuff

--- app/src/main/java/com/example/zx/joke/DelayedMessageService.java
+++ app/src/main/java/com/example/zx/joke/DelayedMessageService.java
@@ -2,13 +2,21 @@ package com.example.zx.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.util.Log;
-
+import android.os.Handler;
+import android.widget.Toast;
 public class DelayedMessageService extends IntentService {
     public static final String EXTRA_MESSAGE = "message";
+    private Handler handler;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
+
     }
 
     @Override
@@ -25,7 +33,12 @@ public class DelayedMessageService extends IntentService {
 
     }
     private void showText(final String text) {
-        Log.v("DelayedMessageService", "The message is: " + text);
+        handler.post(new Runnable() {
+            @Override
+                public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
