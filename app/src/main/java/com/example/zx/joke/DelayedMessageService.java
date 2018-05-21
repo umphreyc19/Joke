422709b0f97fc43203137f1c1735e179a798973c
Collin Beauchamp-Umphrey <umphreyc19@hanover.edu>
2018-05-20 21:06:43 -0400

    Finished

--- app/src/main/java/com/example/zx/joke/DelayedMessageService.java
+++ app/src/main/java/com/example/zx/joke/DelayedMessageService.java
@@ -1,23 +1,22 @@
 package com.example.zx.joke;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
-import android.widget.Toast;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
 public class DelayedMessageService extends IntentService {
     public static final String EXTRA_MESSAGE = "message";
-    private Handler handler;
+    public static final int NOTIFICATION_ID = 5453;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        handler = new Handler();
-        return super.onStartCommand(intent, flags, startId);
 
-    }
 
     @Override
     protected void onHandleIntent(Intent intent) {
@@ -33,12 +32,23 @@ public class DelayedMessageService extends IntentService {
 
     }
     private void showText(final String text) {
-        handler.post(new Runnable() {
-            @Override
-                public void run() {
-                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
-            }
-        });
+        Intent intent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(pendingIntent)
+                .setContentText(text)
+                .build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, notification);
     }
 
 }
